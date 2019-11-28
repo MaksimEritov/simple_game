0902fda58b46dd1acbd0cc23fcf5b9b583555ddc
Kain Wesker <turik3@gmail.com>
2019-11-28 17:36:59 +0400

    final

--- db/dbconection.js
+++ db/dbconection.js
@@ -7,10 +7,11 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://node-chat:fefdi2ki@ds051841.mlab.com:51841/heroku_ncl8xtb4', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://node-chat:fefdi2ki@chat-mjstk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
 
 module.exports = mongoose
+
